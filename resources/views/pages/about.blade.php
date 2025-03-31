2412e3b99627a5650f18f9caba8ec6f079262a8a
Alex de 0. Vidal <contato@webav.com.br>
2025-03-31 08:51:30 -0300

    Add resources/views/pages/about.blade.php

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'About')
+
+@section('content')
+    <h2 class="text-2xl font-bold mb-4">About Us</h2>
+    <p>Welcome to our virtual store! We offer the best products just for you.</p>
+@endsection
