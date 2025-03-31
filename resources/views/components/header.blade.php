3668e1dd7ff5c1fd7cdacd4c98323a91f6654367
Alex de 0. Vidal <contato@webav.com.br>
2025-03-31 08:51:27 -0300

    Add resources/views/components/header.blade.php

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,11 @@
+<header class="bg-white shadow">
+    <div class="container mx-auto p-4">
+        <h1 class="text-xl font-bold">Virtual Store</h1>
+        <nav class="mt-2">
+            <a href="{{ route('home') }}" class="text-gray-700 hover:text-gray-900">Home</a>
+            <a href="{{ route('products.index') }}" class="ml-4 text-gray-700 hover:text-gray-900">Products</a>
+            <a href="{{ route('contact') }}" class="ml-4 text-gray-700 hover:text-gray-900">Contact</a>
+            <a href="{{ route('about') }}" class="ml-4 text-gray-700 hover:text-gray-900">About</a>
+        </nav>
+    </div>
+</header>
