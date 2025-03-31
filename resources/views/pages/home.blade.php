8119af72ad8cad6bd0c66e8ad04ed2061afabf4b
Alex de 0. Vidal <contato@webav.com.br>
2025-03-31 08:51:29 -0300

    Add resources/views/pages/home.blade.php

---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+
+@section('title', 'Home')
+
+@section('content')
+    <h2 class="text-2xl font-bold mb-4">Welcome to Our Store!</h2>
+    <p class="mb-4">Discover our latest products and offers.</p>
+@endsection
